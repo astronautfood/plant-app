e9dd9673cea7dd4fbe1ecc011ad3bc6c9a694390
Dana Manoli <dmanoli1@gmail.com>
2019-09-14 14:36:50 +0200

    app.js hooked up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import firebase from './firebase';
+import { HashRouter as Router, Route } from "react-router-dom";
+import MainView from './components/MainView.js';
 import './styles/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    plants: []
+  }
+
+  componentDidMount() {
+    const dbRef = firebase.database().ref();
+
+    dbRef.on('value', (response) => {
+      console.log(response.val());
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="app-wrapper">
+          <Router basename={"/"}>
+            <Route path="/" component={MainView}/>
+          </Router>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
